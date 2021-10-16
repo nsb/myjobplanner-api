310324c9d6595deed4e6dcd95610e1476a79076a
Niels Sandholt Busch <niels.busch@gmail.com>
2021-10-16 19:57:36 +0200

    Skip useless variable

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,7 @@ import injector from './container'
 const app = express()
 app.use(express.json())
 
-const businessRouter = injector.injectFunction(BusinessRouter)
-
-app.use('/businesses', businessRouter)
+app.use('/businesses', injector.injectFunction(BusinessRouter))
 
 const server = app.listen(3000, () => {
   console.log('The application is listening on port 3000!');
