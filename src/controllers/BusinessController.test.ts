e6ed0efee1a3cd5c236574b2ab7b2aff5cfd4ac5
Niels Sandholt Busch <niels.busch@gmail.com>
2021-10-16 21:54:59 +0200

    Test return value

--- src/controllers/BusinessController.test.ts
+++ src/controllers/BusinessController.test.ts
@@ -5,10 +5,21 @@ import type { Options } from 'express-jwt'
 import { IDbPool } from '../postgres'
 import BusinessRouter, { BusinessController } from './BusinessController'
 import app from '../app'
+import exp from 'constants'
 
 class FakeDb implements IDbPool {
   async query(sql: string) {
-    return Promise.resolve({ rows: [] })
+    return Promise.resolve({
+      rows: [{
+        "id": 1,
+        "name": "Idealrent",
+        "timezone": "Europe/Copenhagen",
+        "country_code": "da",
+        "vat_number": null,
+        "email": "niels.busch@gmail.com",
+        "created": "2021-10-12T06:48:57.616Z"
+      }]
+    })
   }
 }
 
@@ -31,4 +42,13 @@ test('should return a list of businesses', async () => {
   const res = await request(app)
     .get('/businesses').send()
   expect(res.statusCode).toEqual(200)
+  expect(res.body).toEqual([{
+    "id": 1,
+    "name": "Idealrent",
+    "timezone": "Europe/Copenhagen",
+    "country_code": "da",
+    "vat_number": null,
+    "email": "niels.busch@gmail.com",
+    "created": "2021-10-12T06:48:57.616Z"
+  }])
 })
