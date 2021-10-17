f7fdb77654663e38f534d22d2118a3f9057fbea5
Niels Sandholt Busch <niels.busch@gmail.com>
2021-10-17 18:27:58 +0200

    Fix tests

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
-import app from './app'
+import container from './container'
+import BusinessController from './controllers/business.controllers'
+import createApp from './app'
+
+const app = createApp({
+  businessController: container.injectClass(BusinessController)
+})
 
 const server = app.listen(3000, () => {
   console.log('The application is listening on port 3000!');
