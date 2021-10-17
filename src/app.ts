2eb552a77a0c9a5c0d9801d1a264516924003674
Niels Sandholt Busch <niels.busch@gmail.com>
2021-10-17 17:25:20 +0200

    Move app config to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,34 @@
 import express from 'express'
+import path from 'path'
+import { initialize } from 'express-openapi'
+import V1ApiDoc from './api-doc'
+import container from './container'
+import swaggerUi from 'swagger-ui-express'
+import BusinessController from './controllers/business.controllers'
 
 const app = express()
 app.use(express.json())
+
+initialize({
+  apiDoc: V1ApiDoc,
+  app,
+  paths: path.resolve(__dirname, './routes'),
+  dependencies: {
+    businessController: container.injectClass(BusinessController)
+  },
+  routesGlob: '**/*.{ts,js}',
+  routesIndexFileRegExp: /(?:index)?\.[tj]s$/
+})
+
+app.use(
+  "/api-documentation",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "http://localhost:3000/api-docs",
+    },
+  })
+);
+
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,4 @@
-import path from 'path'
-import { initialize } from 'express-openapi'
-import V1ApiDoc from './api-doc'
 import app from './app'
-import container from './container'
-import swaggerUi from 'swagger-ui-express'
-import BusinessController from './controllers/business.controllers'
-
-initialize({
-  apiDoc: V1ApiDoc,
-  app,
-  paths: path.resolve(__dirname, './routes'),
-  dependencies: {
-    businessController: container.injectClass(BusinessController)
-  },
-  routesGlob: '**/*.{ts,js}',
-  routesIndexFileRegExp: /(?:index)?\.[tj]s$/
-})
-
-app.use(
-  "/api-documentation",
-  swaggerUi.serve,
-  swaggerUi.setup(undefined, {
-    swaggerOptions: {
-      url: "http://localhost:3000/api-docs",
-    },
-  })
-);
 
 const server = app.listen(3000, () => {
   console.log('The application is listening on port 3000!');
