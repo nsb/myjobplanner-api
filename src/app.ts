483fb1afcdc0d62fdd81433ed351197aa93c66a7
Niels Sandholt Busch <niels.busch@gmail.com>
2021-10-23 01:54:53 +0200

    Use JSON api spec

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,14 @@
 import express from 'express'
-import path from 'path/posix'
 import swaggerUi from 'swagger-ui-express'
+import { apiSpec } from './openapi'
 
 const app = express()
 app.use(express.json())
 
-const spec = path.join(__dirname, '../openapi.yaml');
-app.use('/api-docs', express.static(spec));
-
 app.use(
-  "/api-documentation",
+  "/",
   swaggerUi.serve,
-  swaggerUi.setup(undefined, {
-    swaggerOptions: {
-      url: "http://localhost:3000/api-docs"
-    }
-  })
+  swaggerUi.setup(apiSpec)
 )
 
 export default app
--- src/openapi.ts
+++ src/openapi.ts
@@ -1,9 +1,150 @@
 import path from 'path/posix'
 import * as OpenApiValidator from 'express-openapi-validator'
+import type { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types'
+
+export const apiSpec: OpenAPIV3.Document = {
+  openapi: '3.0.1',
+  info: {
+    version: '1.0.0',
+    title: 'myJobPlanner API',
+    description: 'The myJobPlanner API',
+  },
+  servers: [
+    { url: '/v1' }
+  ],
+  paths: {
+    '/businesses': {
+      get: {
+        description: 'Returns all businesses',
+        operationId: 'findBusinesses',
+        parameters: [{
+          name: 'type',
+          in: 'query',
+          description: 'maximum number of results to return',
+          required: false,
+          schema: {
+            type: 'string',
+            enum: [
+              'dog',
+              'cat'
+            ]
+          }
+        }
+          //         - name: tags
+          //           in: query
+          //           description: tags to filter by
+          //           required: false
+          //           style: form
+          //           schema:
+          //             type: array
+          //             items:
+          //               type: string
+          //         - name: limit
+          //           in: query
+          //           description: maximum number of results to return
+          //           required: false
+          //           schema:
+          //             type: integer
+          //             format: int32
+          //             minimum: 1
+          //             maximum: 20
+        ],
+        responses: {
+          '200': {
+            description: 'business response',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'array',
+                  items: {
+                    $ref: '#/components/schemas/Business'
+                  }
+                }
+              }
+            }
+          },
+          default: {
+            description: 'unexpected error',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error'
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+  },
+  components: {
+    schemas: {
+      Business: {
+        type: 'object',
+        required: [
+          'id',
+          'name',
+          'timezone',
+        ],
+        properties: {
+          id: {
+            readOnly: true,
+            type: 'number'
+          },
+          name: {
+            type: 'string'
+          },
+          timezone: {
+            type: 'string'
+          }
+          //         # tag:
+          //         #   type: string
+          //         # type:
+          //         #   $ref: '#/components/schemas/PetType'
+        }
+      },
+      //     # PetType:
+      //     #   type: string
+      //     #   enum:
+      //     #     - dog
+      //     #     - cat
+
+      Error: {
+        type: 'object',
+        required: [
+          'code',
+          'message'
+        ],
+        properties: {
+          code: {
+            type: 'integer',
+            format: 'int32'
+          },
+          message: {
+            type: 'string'
+          }
+        }
+      }
+    },
+    securitySchemes: {
+      ApiKeyAuth: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'X-API-Key',
+      }
+    }
+  }
+}
+
 
 const openApi = OpenApiValidator.middleware({
-  apiSpec: path.join(__dirname, '../openapi.yaml'),
-  validateResponses: true
+  apiSpec: apiSpec,
+  validateRequests: {
+    removeAdditional: true,
+  },
+  validateResponses: {
+    removeAdditional: true,
+  }
 })
 
 export default openApi
