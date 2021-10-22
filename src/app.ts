8d26c1b40884f46b34002b396351fc811119e370
Niels Sandholt Busch <niels.busch@gmail.com>
2021-10-23 00:18:56 +0200

    Add swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import express from 'express'
+import path from 'path/posix'
+import swaggerUi from 'swagger-ui-express'
 
 const app = express()
 app.use(express.json())
+
+const spec = path.join(__dirname, '../openapi.yaml');
+app.use('/api-docs', express.static(spec));
+
+app.use(
+  "/api-documentation",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "http://localhost:3000/api-docs"
+    }
+  })
+)
 
 export default app
