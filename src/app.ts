05a087f4f8ae321afe982bdb410fd55fa6841f62
Niels Sandholt Busch <niels.busch@gmail.com>
2021-10-23 16:11:48 +0200

    Move api docs to root route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,6 @@
 import express from 'express'
-import swaggerUi from 'swagger-ui-express'
-import { apiSpec } from './openapi'
 
 const app = express()
 app.use(express.json())
-
-app.use(
-  "/",
-  swaggerUi.serve,
-  swaggerUi.setup(apiSpec)
-)
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
+import swaggerUi from 'swagger-ui-express'
+import { apiSpec } from './openapi'
 import app from './app'
 import BusinessRouter from './routes/business.routes'
 import container from './container'
 
 app.use('/v1/businesses', container.injectFunction(BusinessRouter))
+
+app.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(apiSpec)
+)
 
 const server = app.listen(3000, () => {
   console.log('The application is listening on port 3000!');
