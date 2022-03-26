228c1c3c0765d21d2a7d58bf4bc0fba6d406fedc
Niels Sandholt Busch <niels.busch@gmail.com>
2022-03-26 18:41:04 +0100

    Remove unused code

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import * as s from 'zapatos/schema';
 import { IRepository } from '../repositories/BaseRepository'
-import type { ApiEnvelope, QueryParams, ITransformer } from '../types'
+import type { ITransformer } from '../types'
 
 export abstract class BaseController<Insertable, Selectable, Whereable, Table extends s.Table, DTO> {
   constructor(
@@ -22,31 +22,6 @@ export abstract class BaseController<Insertable, Selectable, Whereable, Table ex
       }
     }
   }
-
-  //   async getClients(req: Request<unknown, unknown, unknown, ClientQueryParams>, res: Response<ApiEnvelope<ClientDTO>>, next: NextFunction): Promise<void> {
-  //     if (req.user) {
-  //       try {
-  //         const offset = parseInt(req.query.offset || "0", 10)
-  //         const limit = parseInt(req.query.limit || "20", 10)
-  //         const orderBy = 'created'
-  //         const orderDirection = 'ASC'
-  //         const where: s.clients.Whereable = {}
-  //         if (req.query.businessId) {
-  //           where.business_id = req.query.businessId
-  //         }
-  //         const { totalCount, result } = await this.repository.find(req.user.sub, where, { limit, offset, orderBy, orderDirection })
-
-  //         res.status(200).json({
-  //           data: result.map(this.transformer.serialize),
-  //           meta: {
-  //             totalCount
-  //           }
-  //         })
-  //       } catch (err) {
-  //         next(err)
-  //       }
-  //     }
-  //   }
 }
 
 export default BaseController
