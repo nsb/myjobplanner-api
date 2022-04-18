e9b1402c93e2392fe807a39a0d1e6b6a408cc280
Niels Sandholt Busch <niels.busch@gmail.com>
2022-04-18 15:06:12 +0200

    Use service

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -1,49 +1,10 @@
-import { Pool } from 'pg'
-import * as db from 'zapatos/db'
 import * as s from 'zapatos/schema'
-import type { IService } from './base.service'
-import type { IClientRepository } from '../repositories/ClientRepository'
-import { RepositoryOptions } from '../types'
+import { BaseService, IService } from './base.service'
 
 export interface IClientService extends IService<s.clients.Insertable, s.clients.Updatable, s.clients.JSONSelectable, s.clients.Whereable, s.clients.Table> {}
 
-class ClientService implements IClientService {
-    public static inject = ['pool', 'clientRepository'] as const;
-    constructor (
-        private pool: Pool,
-        private clientRepository: IClientRepository
-    ) {}
-
-    async create (userId: string, business: s.clients.Insertable) {
-      return db.readCommitted(this.pool, async txnClient => {
-        return this.clientRepository.create(userId, business, undefined, txnClient)
-      })
-    }
-
-    async update (userId: string, id: number, business: s.businesses.Updatable, businessId?: number) {
-      return db.readCommitted(this.pool, async txnClient => {
-        return this.clientRepository.update(
-          userId,
-          id,
-          business,
-          businessId,
-          txnClient
-        )
-      })
-    }
-
-    async find (
-      userId: string,
-      where: s.clients.Whereable,
-      { limit, offset, orderBy, orderDirection }: RepositoryOptions<s.clients.Table>,
-      businessId?: number
-    ) {
-      return this.clientRepository.find(userId, where, { limit, offset, orderBy, orderDirection }, businessId)
-    }
-
-    async get (userId: string, id: number) {
-      return this.clientRepository.get(userId, id)
-    }
+class ClientService extends BaseService<s.clients.Insertable, s.clients.Updatable, s.clients.JSONSelectable, s.clients.Whereable, s.clients.Table> {
+  public static inject = ['pool', 'clientRepository'] as const;
 }
 
 export default ClientService
