8283c99d00495b3f1a5abdf6c8a2dd93eff83e8c
Niels Sandholt Busch <niels.busch@gmail.com>
2022-05-01 14:10:46 +0200

    Visit route tests

---
+++ src/routes/visit.routes.test.ts
@@ -0,0 +1,170 @@
+import express from 'express'
+import { createInjector } from 'typed-inject'
+import request from 'supertest'
+import type { Request, Response, NextFunction } from 'express'
+import type { Options } from 'express-jwt'
+import type { IVisitService } from '../services/visit.service'
+import VisitController from '../controllers/visit.controllers'
+import VisitRouter from './visit.routes'
+import openApi from '../openapi'
+
+function authorizationMiddleware (...permittedRoles: string[]) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    next()
+  }
+}
+
+function jwtMiddleware (req: Request, res: Response, next: NextFunction) {
+  req.user = {
+    iss: '',
+    sub: 'abc',
+    aud: [],
+    iat: 1,
+    exp: 1,
+    azp: '',
+    scope: ''
+  }
+  next()
+}
+jest.mock('express-jwt', () => { return (options: Options) => { return jwtMiddleware } })
+jest.mock('express-jwt-authz', () => { return (options: Options) => { return jwtMiddleware } })
+
+describe('VisitController', () => {
+  test('GET /v1/businesses/:businessId/visits', async () => {
+    const mockedResult = [
+      1,
+      [
+        [{
+          id: 1,
+          job_id: 1,
+          invoice_id: null,
+          completed: false,
+          begins: '2021-11-11T22:55:57.405524',
+          ends: null,
+          anytime: true,
+          created: '2021-11-11T22:55:57.405524'
+        }, []]
+      ]
+    ]
+
+    const MockService = jest.fn<IVisitService, []>(() => ({
+      find: jest.fn().mockResolvedValue(mockedResult),
+      get: jest.fn(),
+      create: jest.fn().mockResolvedValueOnce({}),
+      update: jest.fn()
+    }))
+
+    const container = createInjector()
+      .provideClass('visitService', MockService)
+      .provideClass('visitController', VisitController)
+      .provideValue('authorization', authorizationMiddleware)
+      .provideValue('openApi', openApi)
+
+    const app = express()
+    app.use(express.json())
+    app.use('/v1', container.injectFunction(VisitRouter))
+
+    const res = await request(app)
+      .get('/v1/businesses/1/visits').send()
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual({
+      data: [{
+        id: 1,
+        jobId: 1,
+        invoiceId: null,
+        completed: false,
+        begins: '2021-11-11T22:55:57.405524',
+        ends: null,
+        anytime: true,
+        lineItems: []
+      }],
+      meta: {
+        totalCount: 1
+      }
+    })
+  })
+
+  test('GET /v1/businesses/:businessId/visits not found', async () => {
+    const mockedQueryResult = [0, []]
+
+    const MockService = jest.fn<IVisitService, []>(() => ({
+      find: jest.fn().mockResolvedValue(mockedQueryResult),
+      get: jest.fn(),
+      create: jest.fn().mockResolvedValue({}),
+      update: jest.fn()
+    }))
+
+    const container = createInjector()
+      .provideClass('visitService', MockService)
+      .provideClass('visitController', VisitController)
+      .provideValue('authorization', authorizationMiddleware)
+      .provideValue('openApi', openApi)
+
+    const app = express()
+    app.use(express.json())
+    app.use('/v1', container.injectFunction(VisitRouter))
+
+    const res = await request(app)
+      .get('/v1/businesses/1/visits').send()
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual({
+      data: [],
+      meta: {
+        totalCount: 0
+      }
+    })
+  })
+
+  test('POST /v1/businesses/:businessId/visits', async () => {
+    const mockedQueryResult = [{
+      id: 1,
+      job_id: 1,
+      invoice_id: null,
+      completed: false,
+      begins: '2021-11-11T22:55:57.405524',
+      ends: null,
+      anytime: true,
+      created: '2021-11-11T22:55:57.405524'
+    }, []]
+
+    const MockService = jest.fn<IVisitService, []>(() => ({
+      find: jest.fn(),
+      get: jest.fn(),
+      create: jest.fn().mockResolvedValue(mockedQueryResult),
+      update: jest.fn()
+    }))
+
+    const container = createInjector()
+      .provideClass('visitService', MockService)
+      .provideClass('visitController', VisitController)
+      .provideValue('authorization', authorizationMiddleware)
+      .provideValue('openApi', openApi)
+
+    const app = express()
+    app.use(express.json())
+    app.use('/v1', container.injectFunction(VisitRouter))
+
+    const res = await request(app)
+      .post('/v1/businesses/1/visits')
+      .send({
+        jobId: 1,
+        invoiceId: null,
+        completed: false,
+        begins: '2021-11-11T22:55:57.405524',
+        ends: null,
+        anytime: true,
+        lineItems: []
+      })
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual({
+      id: 1,
+      jobId: 1,
+      invoiceId: null,
+      completed: false,
+      begins: '2021-11-11T22:55:57.405524',
+      ends: null,
+      anytime: true,
+      lineItems: []
+    })
+  })
+})
--- src/services/visit.service.ts
+++ src/services/visit.service.ts
@@ -1,9 +1,18 @@
 import * as s from 'zapatos/schema'
-import { BaseService } from './base.service'
+import { BaseService, IService } from './base.service'
 
 type VisitInsertable = [s.visits.Insertable, s.lineitems.Insertable[]]
 type VisitUpdatable = [s.visits.Updatable, (s.lineitems.Updatable | s.lineitems.Insertable)[]]
 type VisitSelectable = [s.visits.JSONSelectable, s.lineitems.JSONSelectable[]]
+type VisitWhereable = [s.visits.Whereable, s.clients.Whereable]
+
+export interface IVisitService extends IService<
+  VisitInsertable,
+  VisitUpdatable,
+  VisitSelectable,
+  VisitWhereable,
+  s.visits.Table
+> {}
 
 class VisitService extends BaseService<
   VisitInsertable,
